bb29dc4059c16262a4aa1360ccb52a36dd57150a
Jonibek <munirovjonibek@gmail.com>
2022-03-29 11:09:56 +0500

    finished

--- script.js
+++ script.js
@@ -6,22 +6,27 @@ let a = 0;
 let row = 0;
 let list = document.querySelector(".list");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
   a = input.value;
-  row = `<div class="todo">
-  <p class="todo_text">${a}</p>
-  <button class="todo__btn">Delete</button>
-</div>`;
-  list.insertAdjacentHTML("afterbegin", row);
-  console.log(a);
+  //   row = `<div class="todo">
+  //   <p class="todo_text">${a}</p>
+  //   <button class="todo__btn">Delete</button>
+  // </div>`;
+  //   list.insertAdjacentHTML("afterbegin", row);
+  let element = document.createElement("div");
+  element.classList.add("todo");
+  let elementText = document.createElement("p");
+  elementText.classList.add("todo_text");
+  elementText.textContent = a;
+  let elementBtn = document.createElement("button");
+  elementBtn.classList.add("todo__btn");
+  elementBtn.textContent = "Delete";
+  element.append(elementText);
+  element.append(elementBtn);
+  list.prepend(element);
   input.value = "";
-});
-
-for (let el of btn__del) {
-  el.addEventListener("click", function (e) {
-    if (e.target.classList.contains("todo__btn")) {
-      e.target.parentElement.remove();
-      console.log(btn__del);
-    }
+  elementBtn.addEventListener("click", function (e) {
+    element.remove();
   });
-}
+});

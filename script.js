636e729edbe8633b98d9f5097968895067247458
Jonibek <munirovjonibek@gmail.com>
2022-03-29 11:33:33 +0500

    updated and cleancode

--- script.js
+++ script.js
@@ -6,22 +6,19 @@ let a = 0;
 let row = 0;
 let list = document.querySelector(".list");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
   a = input.value;
   row = `<div class="todo">
   <p class="todo_text">${a}</p>
   <button class="todo__btn">Delete</button>
 </div>`;
   list.insertAdjacentHTML("afterbegin", row);
-  console.log(a);
   input.value = "";
 });
 
-for (let el of btn__del) {
-  el.addEventListener("click", function (e) {
-    if (e.target.classList.contains("todo__btn")) {
-      e.target.parentElement.remove();
-      console.log(btn__del);
-    }
-  });
-}
+list.addEventListener("click", function (e) {
+  if (e.target.classList.contains("todo__btn")) {
+    e.target.parentElement.remove();
+  }
+});
